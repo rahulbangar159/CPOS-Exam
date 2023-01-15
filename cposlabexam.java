95086c728b439e2120eeee7845578873b63c72fa
rahulbangar159 <122715737+rahulbangar159@users.noreply.github.com>
2023-01-15 12:11:01 +0530

    Rahul Bangar PRN 220960920011 CPOS lab exam file

---
+++ cposlabexam.java
@@ -0,0 +1,14 @@
+public class cposlabexam {
+
+	public static void main(String[] args)
+
+	{
+		int i, j, n = 4;
+
+		for (i = 1; i <= n; i++) {
+			for (j = 1; j <= i; j++)
+				System.out.print(i);
+			System.out.println("");
+		}
+	}
+}
